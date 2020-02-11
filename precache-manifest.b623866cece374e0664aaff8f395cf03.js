1d0ced9f77b11406f22bbf483e9a70b82573e01c
RubenV-Dev <r.will16@hotmail.com>
2020-02-11 12:10:38 -0500

    Updates

---
+++ precache-manifest.b623866cece374e0664aaff8f395cf03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24c6d45de739ab6e275ef893212732a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82be156e56a983af173d",
+    "url": "/static/css/main.588aebca.chunk.css"
+  },
+  {
+    "revision": "0ea1e739e21457729e2d",
+    "url": "/static/js/2.0f15e47f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0f15e47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82be156e56a983af173d",
+    "url": "/static/js/main.6a4fa5fb.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
