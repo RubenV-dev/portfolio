0d8c39d43d6010aabb8abdbafa7ae183345dfbcf
RubenV-Dev <r.will16@hotmail.com>
2020-02-11 12:14:53 -0500

    Updates

---
+++ precache-manifest.1b2fcdfa1221824843556e51cbc56aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f384d6b528f27ee5019f88818f784bf9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "786cd114aa582a56a01a",
+    "url": "/portfolio/static/css/main.588aebca.chunk.css"
+  },
+  {
+    "revision": "0ea1e739e21457729e2d",
+    "url": "/portfolio/static/js/2.0f15e47f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.0f15e47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786cd114aa582a56a01a",
+    "url": "/portfolio/static/js/main.620dc62a.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
